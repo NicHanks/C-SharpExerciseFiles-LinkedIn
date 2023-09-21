ab2280b65410f1a788ae064a215417ebe04703c1
Nick <nikhanks@gmail.com>
2023-09-20 23:13:22 -0600

    update ALL

---
+++ Overview/VarsAndData/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace VarsAndData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare some basic value type variables
+            int i = 10;
+            float f = 2.0f;
+            decimal d = 10.0m;
+            bool b = true;
+            char c = 'c';
+
+            // Declare a string - it's a collection of characters
+            string str = "a string";
+
+            // Declare an implicit variable
+            var x = 10;
+            var z = "Hello!";
+
+            // TODO: Declare an array of values
+            string[] str_array = {"hourse", "red", "bachelorette"};
+            int[] vals = new int[5];
+            // TODO: Print the values using a Formatting String
+            Console.WriteLine("{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}",i,c,b,str,f,d,x,z);
+
+            // TODO: "null" means "no value"
+            object obj = null;
+            Console.WriteLine(obj);
+
+            // TODO: Implicit conversion between types
+            Console.WriteLine(obj);
+            long bignum; 
+                        // Console.WriteLine(i.GetType());
+            bignum = i;
+            //             Console.WriteLine(i);
+            // object objct = null;
+            //     Console.WriteLine(bignum.GetType());
+
+
+
+            // TODO: Explicit conversions
+            float i_to_f = (float)i;
+            Console.WriteLine("{0}", i_to_f);
+
+            int f_to_i = (int)f;
+            Console.WriteLine("{0}", f_to_i);
+
+            // float hulah = (float)i;
+            // Console.WriteLine(hulah);
+        }
+    }
+}
---
+++ Program Flow/ForLoops/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ForLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVal = 15;
+            int[] nums = new int[] {3, 14, 15, 92, 6};
+            string str = "The quick brown fox jumps over the lazy dog";
+
+            // TODO: the basic for loop
+            for (int i = 38; 15 == myVal; i + 3)
+            Console.WriteLine("The basic for loop:");
+            for (int i = 0; i < myVal; i++) {
+                Console.WriteLine("i is currently {0}", i);
+            }
+
+            Console.WriteLine();
+
+            //for (int i = 0; i < myVal; i++) 
+
+            // TODO: the foreach-in loop can be used to iterate over sequences
+            // Console.WriteLine("The foreach loop:");
+
+
+            // TODO: count the number of o's in the string
+
+        }
+    }
+}
